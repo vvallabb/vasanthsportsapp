52e7dd8e1db96257986f3e30574a054523dce627
vasanth naib <vasanth.naib@sap.com>
2012-07-29 10:15:51 +0530

    Change-Id: I912ee689758d445b07b4ea48ff274ca0efedb636 Signed-off-by: vvallabb

---
+++ cricketers/src/com/vasanth/utils/FileCricketersRead.java
@@ -0,0 +1,39 @@
+package com.vasanth.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileCricketersRead {
+
+	/**
+	 * @param args
+	 */
+
+ 
+	public static void main(String[] args) {
+ 
+		BufferedReader br = null;
+ 
+		try {
+ 
+			String sCurrentLine;
+ 
+			br = new BufferedReader(new FileReader("C:\\Users\\i068090\\Desktop\\separate.html"));
+ 
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+ 
+	}
+}
